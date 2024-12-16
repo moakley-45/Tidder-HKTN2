f950ba03e2b96affab55034f6ede6a2e1c0053ab
Mars Oakley <a.oakley4589@gmail.com>
2024-12-16 13:38:58 +0000

    hooked up Cloudinary

--- tidder/settings.py
+++ tidder/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
+    'home',
+    'content',
+    'search,'
 ]
 
 MIDDLEWARE = [
